b7c9f7452a2bc3ef3f9b859c0960b228b4660e77
Bradley Martin <bradley.r.martin@me.com>
2022-10-30 14:48:17 +1100

    feat(linter): adds linter

--- src/hocs/useBridge.tsx
+++ src/hocs/useBridge.tsx
@@ -1,14 +1,15 @@
 import Bridge from '../components/Bridge/Bridge'
 import React from 'react'
 
-export default function useBridge<T>(Component: React.ComponentType<T>) {
-  return (props: T) => {
+export default function useBridge<T extends object>(Component: React.ComponentType<T>) {
+  const WrapperComponent = (props: T) => {
     const { ...native } = props
     return (
       <Bridge>
-        {/* @ts-ignore */}
         <Component {...native} />
       </Bridge>
     )
   }
+  WrapperComponent.displayName = 'useBridge(WrapperComponent)'
+  return WrapperComponent
 }
--- src/hocs/useController.tsx
+++ src/hocs/useController.tsx
@@ -1,14 +1,18 @@
 import Controller from '../components/Controller/Controller'
 import React from 'react'
 
-export default function useController<T>(controller: string, Component: React.ComponentType<T>) {
-  return (props: T) => {
+export default function useController<T extends object>(
+  controller: string,
+  Component: React.ComponentType<T>,
+) {
+  const WrappedComponent = (props: T) => {
     const { ...native } = props
     return (
       <Controller controller={controller}>
-        {/* @ts-ignore */}
         <Component {...native} />
       </Controller>
     )
   }
+  WrappedComponent.displayName = 'useController(WrappedComponent)'
+  return WrappedComponent
 }
